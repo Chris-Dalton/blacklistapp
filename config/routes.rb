d3d954624fddb11067a5debbdeb929c2ff96dd10
Chris Dalton <chris.dalton1207@gmail.com>
2017-06-08 17:46:36 +0000

    created search, about, and submit views with routing. Added gradient to background. Created navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
  root to: 'pages#home'
  get 'about', to: 'pages#about'
+ get 'submit', to: 'pages#submit'
+ get 'search', to: 'pages#search'
+ post 'users', to: 'users#create'
+ 
+ resources :users
+ 
 end

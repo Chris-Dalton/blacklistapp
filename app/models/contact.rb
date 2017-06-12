33e9145f5a999de2175e576c4e6b889b64204ddc
Chris Dalton <chris.dalton1207@gmail.com>
2017-06-12 15:53:59 +0000

    added data validation to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-    
+  validates :first_Name, presence: true
+  validates :last_Name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end

7f6a3fcb9f5112d3c6a3cf25d4dc9b18a22cedf7
Chris Dalton <chris.dalton1207@gmail.com>
2017-06-12 18:07:10 +0000

    improved flash messages and fixed an error in contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,17 +7,17 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
       if @contact.save
-        flash[:success] = "Message sent."
+        flash[:success] = "Message sent"
         redirect_to new_contact_path
       else
-        flash[:error] = @contact.errors.full_messages.join(", ")
+        flash[:danger] = @contact.errors.full_messages.join(", ")
         redirect_to new_contact_path
       end
   end
   
   private
   def contact_params
-    params.require(:contact).permit(:name, :email, :comments)
+    params.require(:contact).permit(:first_Name, :last_Name, :email, :comments)
   end
   
   

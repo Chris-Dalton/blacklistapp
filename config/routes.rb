2bb4b96ee7f1d0c3d55627a2868ad19d5fcf381c
Chris Dalton <chris.dalton1207@gmail.com>
2017-06-12 18:13:25 +0000

    Added custom contact form path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
  root to: 'pages#home'
+ 
  get 'about', to: 'pages#about'
  get 'submit', to: 'pages#submit'
  get 'search', to: 'pages#search'
- post 'users', to: 'users#create'
  
  resources :users
+ post 'users', to: 'users#create'
+ 
  resources :contacts
+ get 'contact-us', to: 'contacts#new'
  
 end

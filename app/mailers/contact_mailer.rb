b07d43c12ac811f5903867a0d4fa678c690ca164
Chris Dalton <chris.dalton1207@gmail.com>
2017-06-13 01:32:10 +0000

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,9 @@
 class ContactMailer < ActionMailer::Base
   default to: 'chris@christophermdalton.com'
   
-  def contact_email(name, email, body)
-    @name = name
+  def contact_email(first_Name, last_Name, email, body)
+    @firstname = first_Name
+    @lastname = last_Name
     @email = email
     @body = body
     mail(from: email, subject: 'Contact Form Message')

6926838be48b61579567688ae499f0357d488bd3
Chris Dalton <chris.dalton1207@gmail.com>
2017-06-13 01:47:16 +0000

    added trigger on mailer controller form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,6 +9,11 @@ class ContactsController < ApplicationController
       if @contact.save
         flash[:success] = "Message sent"
         redirect_to new_contact_path
+          first_Name = params[:contact][:first_Name]
+          last_Name = params[:contact][:last_Name]
+          email = params[:contact][:email]
+          body = params[:contact][:comments]
+          ContactMailer.contact_email(first_Name, last_Name, email, body).deliver
       else
         flash[:danger] = @contact.errors.full_messages.join(", ")
         redirect_to new_contact_path
